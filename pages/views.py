f2edaeab50f7f1821707f0bc86782752dcca62c2
jos3alvarado <josealvaradotv625@gmail.com>
2026-03-28 09:44:07 -0400

    about page done

--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomepageView(TemplateView):
     template_name = "home.html"
+
+class AboutView(TemplateView):
+    template_name = "about.html"
